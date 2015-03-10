069e305d3e390cb3013edcadb6a5541d6a76ab9e
Jules Chevalier <jules.chevalier@univ-st-etienne.fr>
2015-03-10 09:15:07 +0100

    New background and new layout

--- js/navigation.js
+++ js/navigation.js
@@ -1,13 +1,3 @@
-function footer()
-{
-		return "<footer>"+
-				"<p>Made by <a href=\"http://twitter.com/chgravier\">@chgravier</a> & <a href=\"http://twitter.com/jsubercaze\">@jsubercaze</a>. Special thanks to <a href=\"http://twitter.com/mehlah\">@mehlah</a> for the HTML/CSS discussions !</p>"+
-					"<p>&copy; <a href=\"http://www.telecom-st-etienne.fr\">Télécom Saint-Etienne</a> - <a href=\"http://portail.univ-st-etienne.fr/\">Université Jean Monnet</a>"+
-					"		- <a href=\"http://www.institut-telecom.fr\">Institut Télécom 2012</a><br />"+
-			" </footer>";
-}
-
-
 function menu()
 {
 
@@ -19,7 +9,6 @@ function menu()
             "<span class=\"icon-bar\"></span>"+
             "<span class=\"icon-bar\"></span>"+
           "</a>"+
-          "<a class=\"brand\" href=\"#\">@jchevalier</a>"+
           "<div class=\"nav-collapse\">"+
             "<ul class=\"nav\">"+
 							"<li id=\"menuhome\"><a href=\"./index.html\">Home</a></li>"+
